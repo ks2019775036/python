bae75a130464dadef2ce2d21f5ca467cc7efd1b5
ks2019775036 <48661705+ks2019775036@users.noreply.github.com>
2019-03-27 16:37:48 +0900

    Add files via upload

---
+++ age.py
@@ -0,0 +1,6 @@
+name = input("이름")
+year = int(input("출생연도"))
+age = 2019-year +1
+
+print(name, "님의 나이는" ,age,"살 입니다.")
+           
---
+++ asmd.py
@@ -0,0 +1,7 @@
+odd = 1+3+5+7+9
+even = 2+4+6+8+10
+diff = even - odd
+
+print("1부터 10까지 홀수들의 합 =", odd)
+print("1부터 10까지 짝수들들의 합 =", even)
+print("1부터 10까지 홀수들의 합 =", odd)
